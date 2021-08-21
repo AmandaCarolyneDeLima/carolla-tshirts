5e473c3a2669c2de917d517dcb22a214c42b51da
AmandaCarolyneDeLima <amandaacarolyne@outlook.com>
2021-08-20 23:13:21 -0300

    terceira prova

--- frontend/assets/resources/scripts/index.js
+++ frontend/assets/resources/scripts/index.js
@@ -2,23 +2,47 @@
 
 /**-----Prompt-----
 -----setTimeOut-----
------Função aninhada/local-----
 -----Tradicional---onload-----
------String template-----*/
+-----String template-----
+-----LocalStorage-----
+-----Leitura e escrita de dados simples (tipos primitivos)-----*/
 
-let nome = prompt('Digite seu nome, por favor:", "nome');
+let nome;
+if (!localStorage.getItem('nome')) {
+    nome = prompt('Digite seu nome, por favor:', 'nome');
+    localStorage.setItem('nome', nome);
+
+} else {
+    nome = localStorage.getItem('nome');
+}
+
+//Leitura e escrita de JSON//
 
 window.onload = function () {
     setTimeout(function () {
         window.alert(`Olá ${nome}, seja bem vinda a nossa página!`);
+        if (localStorage.getItem('ultimoAcesso')) {
+            let ultimoAcesso = JSON.parse(localStorage.getItem('ultimoAcesso'));
+            window.alert(`Seu ultimo acesso foi dia ${ultimoAcesso.dia} do mês ${ultimoAcesso.mes} de ${ultimoAcesso.ano}, às ${ultimoAcesso.hora} horas e ${ultimoAcesso.minuto} minutos.`);
+        }
     }, 100);
 };
+
+let agora = new Date();
+let acesso = {
+    dia: agora.getDate(),
+    mes: agora.getMonth() + 1,
+    ano: agora.getFullYear(),
+    hora: agora.getHours(),
+    minuto: agora.getMinutes()
+};
+
+localStorage.setItem('ultimoAcesso', JSON.stringify(acesso));
 
 //----------
 
 /**-----Evento de movimento do mouse-----
 -----Funcão com nome-----
------Função sem argumento-----
 -----Via seletores CSS usados na função querySelector() ou querySelectorAll()-----*/
 
 function mouseOver() {
